85c54a08b8c44bccc34313fe1df1957bd3478527
Win Chhor <winchhor@gmail.com>
2021-04-15 18:13:37 -0400

    Added producer project.

---
+++ producer/src/main/java/jee3060/producer/model/Hobby.java
@@ -0,0 +1,12 @@
+package jee3060.producer.model;
+
+import lombok.Data;
+
+@Data
+public class Hobby {
+    private int hobbyId;
+    private String hobbyName;
+    private String hobbyDescription;
+    private String hobbyCategory;
+}
+
